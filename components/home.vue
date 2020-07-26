049e2f962e383eea9091f3fe20462034b144089f
AthanatiusC <lexi_anugrah@Hotmail.com>
2020-07-26 12:51:15 +0700

    stuck link

--- components/home.vue
+++ components/home.vue
@@ -1,18 +1,76 @@
 <template>
     <div class="fluid-container" style="margin:20px">
-        <img class="img" v-bind:src="'data:image/gif;base64,'+ imgbase64" alt="Stream">
-        <div class="rec">
-            <div class="result">
-
-            </div>
+        <div class="stream-display" v-for="(stream,key) in streams" :key="stream">
+                <div class="stream-holder" >
+                        <img class="img" draggable="false" v-bind:src="'data:image/gif;base64,'+ stream" alt="Stream">
+                        <h3 class="stream-name">Camera : {{key}}</h3>
+                </div>
+                
+                <NLink :to="{ path: '/streaming', query: { id: '0' }}">
+                GO
+                </NLink>
         </div>
+        <transition-group name="list" tag="p">
+            <div class="pop-up" v-for="mess in message" v-bind:key="mess">
+                <div class="message-title">
+                    <b>Message</b>
+                </div>
+                <div class="divider"></div>
+                <div class="message-body">
+                    <span class="list-item">
+                        {{ mess }}
+                    </span>
+                </div>
+            </div>
+        </transition-group>
     </div>
 </template>
+<style>
+    .img{
+        border-radius: 5px 1px 5px 1px;
+        border-radius: 10px;
+        height:300px;
+        width:400px;
+    }
+    .stream-holder{
+        display:inline-block;
+        border: solid 1px #312f40;
+        text-align: center;
+        border-radius: 5px;
+    }
+    .pop-up{
+        background-color:#292736;
+        padding:5px 10px 5px 10px;
+        margin:20px;
+        display: block;
+        position: fixed;
+        top:0px;
+        left:50%;
+    }
+    .divider{
+        border-bottom:solid #47445c;
+        border-width: thin;
+        margin: 5px 0px 5px 0px;
+    }
+    .message-title{
+        text-align: center;
+    }
+    .list-enter-active, .list-leave-active {
+        transition: all 1s;
+    }
+    .list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+        opacity: 0;
+        transform: translateY(30px);
+    }
+</style>
 <script>
 export default {
-    data :{
-        stream:Array,
-        online_cameras:Array
+    data(){
+        return{
+            online_cameras:"",
+            streams:{},
+            message : []
+        }
     },
     mounted(){
         this.socket = this.$nuxtSocket({
@@ -22,39 +80,32 @@ export default {
     },
     methods:{
         initSocket(){
-            console.log(this.getOnlineCam())
-            for(cam in this.online_cameras){
-                this.socket.emit('stream',cam)
-                this.socket.on('serverstream'+cam,(response)=>{
-                    this.stream[cam] = response
-                })
-            }
-            this.socket.emit('recognized')
+            this.socket.on("online_cameras",(cam_list)=>{
+                for(var cam in cam_list){
+                    this.socket.emit('stream',cam)
+                    this.socket.on('serverstream'+cam,(response)=>{
+                        this.streams = {}
+                        this.streams = {[cam]:response}
+                        // console.log("responded")
+                    })
+                }
+            })
+            this.online_cameras = this.streams.length
+            // this.socket.emit('recognized')
             this.socket.on('result',(result)=>{
                 // console.log(result)
             })
             this.socket.on('message',(result)=>{
-                console.log(result)
+                this.message.push(result)
+                setTimeout(() => {
+                    this.message.pop()
+                }, 3000);
             })
         },
-        getOnlineCam(){
-            var a
-            this.socket.on("online_cameras",(resp)=>{
-                a = resp
-            })
-            setInterval()
+        test(){
+            console.log('test')
         }
     }
 
 }
 </script>
-<style>
-    .img{
-        border-radius: 10px;
-        height:300px;
-        width:400px;
-        margin:0px 10px 0px 10px;
-        border: solid 1px #312f40;
-        padding:3px;
-    }
-</style>
--- pages/streaming.vue
+++ pages/streaming.vue
@@ -1,5 +1,5 @@
 <template>
     <div>
-        
+        {{$route.query}}
     </div>
 </template>
